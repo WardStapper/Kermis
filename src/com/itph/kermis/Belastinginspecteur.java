64b2252a93fa392ffcb74e1d5fdd5c6e2da9ac6c
Ward <Ward_Stapper@hotmail.com>
2018-08-28 16:31:11 +0200

    Something

--- src/com/itph/kermis/Belastinginspecteur.java
+++ src/com/itph/kermis/Belastinginspecteur.java
@@ -8,10 +8,10 @@ public class Belastinginspecteur {
    public void belastingHeffen(ArrayList<Attractie> attractie){
 
         for(int a = 0;a<attractie.size();a++)
-                if (attractie instanceof GokAttractie){
-            ((GokAttractie) attractie).kansSpelBelastingBetaling();
-        }
-       System.out.println("U heeft " + Kassa.belastingen + " moeten betalen!");
+            if (attractie instanceof GokAttractie){
+                ((GokAttractie) attractie).kansSpelBelastingBetaling();
+            }
+        System.out.println("U heeft " + Kassa.belastingen + " moeten betalen!");
 
 
     }
