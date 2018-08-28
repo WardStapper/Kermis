b7e833152b8a0039f6800c35c84948979b95de11
Ward <Ward_Stapper@hotmail.com>
2018-08-28 21:29:53 +0200

    Something

--- src/com/itph/kermis/Bezoek.java
+++ src/com/itph/kermis/Bezoek.java
@@ -2,11 +2,12 @@ package com.itph.kermis;
 
 import java.util.Scanner;
 
-public class Bezoeker {
-
+public class Bezoek {
+        Belastinginspecteur belastingMan = new Belastinginspecteur();
         Scanner sc = new Scanner(System.in);
         Kassa kassa = new Kassa();
         boolean stopKermis = false;
+        boolean correcteKeuze = false;
 
 
     public void bezoekje() {
@@ -14,8 +15,9 @@ public class Bezoeker {
             System.out.println("Welkom op de kermis, maak een keuze uit onderstaand menu: ");
 
             kassa.maakKeuze();
+            belastingMan.randomLangsgaan();
 
-
+        while(!correcteKeuze){
             System.out.println("Toets o voor een overzicht van de totaalomzet. Toets k voor het totaal aantal kaartjes, toets v om verder te gaan, toets q om af te sluiten, b voor belastingdienst");
 
             String input = sc.nextLine();
@@ -23,15 +25,17 @@ public class Bezoeker {
             if (input.equals("o")) {
                 kassa.totaalOmzet();
                 kassa.omzetPerAttractie();
-
+                correcteKeuze = true;
 
             } else if (input.equals("k")) {
                 kassa.totaalRitjes();
                 kassa.ritjesPerAttractie();
                 System.out.println("--------------------------------------");
+                correcteKeuze = true;
 
             } else if (input.equals("v")) {
                 System.out.println("U heeft gekozen om verder te gaan!");
+                correcteKeuze = true;
 
 
             } else if (input.equals("q")) {
@@ -40,10 +44,13 @@ public class Bezoeker {
             } else if (input.equals("b")) {
                 System.out.println("Oké, u heeft de belastinginspecteur aangeroepen!");
                 kassa.belastingKnakkerRoepen();
+                correcteKeuze = true;
             } else {
                 System.out.println("Dit is een ongeldige invoer, probeer nog een keer!");
 
+
             }
+        }
 
         }
     }
